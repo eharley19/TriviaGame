eb0eac0605d00d3b29d3b2656418c419d2957ee2
Edward Harley <edharley19@gmail.com>
2019-11-30 17:56:55 -0500

    fixed timers; questions populate automatically

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@
     var timeToAnswer = 21;
 
     var timer2;
-    var timeToDisplay = 5; 
+    var timeToDisplay = 3; 
 
 $(function () {
 
@@ -40,9 +40,9 @@ $(function () {
         },
 
         {
-        questionText: "Which artist covered the Radiohead classic Creep at Coachella in 2008 and later caused controversy after asking all copies to be removed from the internet?",
-        ansArray: ["Prince", "Kelly Clarkson", "Macy Gray", "The Pretenders"], 
-        rightAnswer: "Prince"
+            questionText: "Which artist covered the Radiohead classic Creep at Coachella in 2008 and later caused controversy after asking all copies to be removed from the internet?",
+            ansArray: ["Prince", "Kelly Clarkson", "Macy Gray", "The Pretenders"], 
+            rightAnswer: "Prince"
 
         }
   
@@ -51,7 +51,9 @@ $(function () {
 
     function questionTimer() {
         clearInterval(timer1);
+        timeToAnswer = 21;
         timer1 = setInterval(ansTimer, 1000);
+        
         
     }   
         
@@ -69,6 +71,7 @@ $(function () {
     function displayTimer() {
         console.log("hello");
         clearInterval(timer2);
+        timeToDisplay = 3;
         timer2 = setInterval(disTimer,1000);
     }
 
@@ -80,6 +83,8 @@ $(function () {
             clearInterval(timer2);
             $("#wrongRight").empty(); 
             asker(currentQuestion);
+            
+                       
             
         }
     }
@@ -115,14 +120,17 @@ $(function () {
     }
 
     function asker(question) {
+
         var displayQuestion = $("<p>").text(question.questionText);
         $("#question").html(displayQuestion);
         var choices = question.ansArray;
         for (i = 0; i < choices.length; i++) {
           var choice =  $("<p>").addClass("clickable").attr("id", i).text(choices[i]);
           $("#answers").append(choice);
+
           
         }
+        questionTimer();
         checkForRightAnswer();
         
     }
